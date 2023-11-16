4ba42106a3f27c646996d57f6fc604e4266ce35a
TOMAS DYLAN LERMAN <47961340@ort>
2023-11-16 15:42:21 -0300

    lalala

--- TP_FINAL/Models/BD.cs
+++ TP_FINAL/Models/BD.cs
@@ -63,7 +63,6 @@ public static class BD
     // SISTEMA KURSED KICKS
 
 
-
     public static List<GENERO> ObtenerGeneros()
     {
         using (SqlConnection db = new SqlConnection(_connectionString))
@@ -124,12 +123,18 @@ public static class BD
     {
       using (SqlConnection db = new SqlConnection(_connectionString))
       {
-         string SQL = "INSERT INTO ";
+        string SP = "SP_InsertarCarrito";      
       }
     }
-    public static List<CARRITO> ObtenerCarrito()
+    public static List<CARRITO> ObtenerCarrito(USUARIO User)
     {
-
+        List<CARRITO> ListaCarrito = null;
+         using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+         string SQL = "SELECT * FROM CARRITO WHERE FkUsuario = @UserId";
+         ListaCarrito = db.Query<CARRITO>(SQL).ToList();
+         return ListaCarrito;
+        }
     }
 
 
